29663ad338ad38b2b32b3ceb067b56ab160b64a9
Alaatohamy <alaatohamy4@gmail.com>
2019-08-27 04:56:22 +0200

    Feat: Add withRouter to card list component to generate dynamic nested url

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './card.style.scss';
 
-const Card = ({title, imageUrl, size, linkUrl}) => {
+const Card = (props) => {
+  const {title, imageUrl, size, linkUrl, history, match} = props;
+
   return (
-    <li className={`card ${size}`}>
+    <li className={`card ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
       <div className="card__background-img" role="presentation" style={{backgroundImage: `url(${imageUrl})` }}></div>
       <div className="card__content">
         <h1 className="title">{title}</h1>
-        <Link to={`/${linkUrl}`} className="subtitle" >Shop Now</Link>
+        <p className="subtitle" >Shop Now</p>
       </div>
     </li>
   )
 }
 
-export default Card;
+export default withRouter(Card);
