c92f821a2fee6109e71fb9c0203bece730723a76
Alaatohamy <alaatohamy4@gmail.com>
2019-09-09 20:53:53 +0200

    Edit: Change the content on the empty state for the drop-down cart and its styling

--- src/components/cart-dropdown/index.js
+++ src/components/cart-dropdown/index.js
@@ -5,20 +5,30 @@ import { Button, CartItem } from 'components';
 import { selectCartItems } from 'redux/cart/cart.selectors';
 import './cart-dropdown.style.scss';
 
-class CartDropdown extends React.Component {
-  render() {
-    const {cartItems} = this.props;
-    return (
-      <div className="cart-dropdown-wrapper">
-        <ul className="cart-dropdown-content">
-          {
-            cartItems.map(item => <CartItem key={item.id} item={item} />)
-          }
-        </ul>
-        <Button text="Go to checkout" color="black" />
-      </div>
-    )
-  }
+const CartDropdown = props => {
+  const {cartItems} = props;
+  return (
+    <div className="cart-dropdown-wrapper">
+      {
+        (cartItems.length)?
+        (
+          <>
+            <ul className="cart-dropdown-content">
+              { cartItems.map(item => <CartItem key={item.id} item={item} />) }
+            </ul>
+            <Button
+              text="Go to checkout"
+              color="black"
+              type="button"
+            />
+          </>
+        )
+        : (
+          <p className="no-data">Your Cart is Empty</p>
+        )
+      }
+    </div>
+  )
 }
 
 const mapState = createStructuredSelector({
