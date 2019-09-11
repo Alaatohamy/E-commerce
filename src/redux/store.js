8e5619d08b9974c1d0f083502289b6af5173484a
Alaatohamy <alaatohamy4@gmail.com>
2019-09-11 05:51:13 +0200

    Refactor: Add logger conditionally according to the env variable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
-// import { createStore, applyMiddleware } from 'redux';
-import { createStore } from 'redux';
-// import { logger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
-// const middleware = [logger];
+const middleware = [];
 
-// const store = createStore(rootReducer, applyMiddleware(...middleware));
-const store = createStore(rootReducer);
+if(process.env.NODE_ENV === 'development'){
+  middleware.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 const persistor = persistStore(store);
  
