fd2f34fd6d6960f7775c274ed9332d08396036a6
Alaatohamy <alaatohamy4@gmail.com>
2019-09-16 00:57:19 +0200

    Edit: route of single collection

--- src/components/collection-section/index.js
+++ src/components/collection-section/index.js
@@ -7,7 +7,7 @@ const CollectionSection = ({collection}) => {
   const {title, routeName, items} = collection;
   return (
     <>
-      <h1 className="collection-title"><Link to={`/${routeName}`}>{title}</Link></h1>
+      <h1 className="collection-title"><Link to={`/shop/${routeName}`}>{title}</Link></h1>
       <ul className="collection-container">
         {items.map((item) => <CardItem key={item.id} item={item} />)}
       </ul>
