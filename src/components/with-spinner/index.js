304221e42c4af57fbe92453be839c987deae4600
Alaatohamy <alaatohamy4@gmail.com>
2019-09-18 01:04:53 +0200

    refactor: Delete console.log for debugging

--- src/components/with-spinner/index.js
+++ src/components/with-spinner/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
 const WithSpinner = ComponentToRender => ({ isLoading, ...otherProps }) => {
-  console.log("withSpinner", isLoading, ComponentToRender);
   return isLoading ? (
     <SpinnerOverlay>
       <SpinnerContainer />
