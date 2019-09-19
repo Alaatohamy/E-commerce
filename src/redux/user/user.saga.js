76286c13963e0a92f764d009435875991dd16f34
Alaatohamy <alaatohamy4@gmail.com>
2019-09-19 14:06:34 +0200

    Refactor: Use one success action creator for both sign-in with email or google Use one failure action creator for both fign-in with email or google Add saga getUserSnapshot to yield it in both email and google sign-in, it handle the common logic between 2 sign-in ways

--- src/redux/user/user.saga.js
+++ src/redux/user/user.saga.js
@@ -1,55 +1,51 @@
 import { all, takeLatest, put } from "redux-saga/effects";
 import { UserActionTypes } from "./user.types";
-import {
-  googleSignInSuccess,
-  googleSignInFailure,
-  emailSignInSuccess,
-  emailSignInFailure
-} from "./user.actions";
+import { signInSuccess, signInFailure } from "./user.actions";
 import {
   auth,
   googleProvider,
   createUserProfileDocument
 } from "firebase-config/firebase.utils";
 
-function* signInWithGoogle() {
+function* getUserSnapshot(user) {
   try {
-    const { user } = yield auth.signInWithPopup(googleProvider);
     const userRef = yield createUserProfileDocument(user);
     const userSnapshot = yield userRef.get();
     /**Add current user to the redux store */
     yield put(
-      googleSignInSuccess({
+      signInSuccess({
         id: userSnapshot.id,
         ...userSnapshot.data()
       })
     );
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error.message));
   }
 }
 
-export function* onGoogleSignInStart() {
-  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
+function* signInWithGoogle() {
+  try {
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    yield getUserSnapshot(user);
+  } catch (error) {
+    yield put(signInFailure(error.message));
+  }
 }
+
 /** Generator function gets the whole action that [onEmailSignIn] listen to and then yield other actions
  * @param {object} action_creator, it gets the whole action, type: EMAIL_SIGN_IN_START
  */
 function* emailSignIn({ payload: { email, password } }) {
   try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
-    const userRef = yield createUserProfileDocument(user);
-    const userSnapshot = yield userRef.get();
-    /**Add current user to the redux store */
-    yield put(
-      emailSignInSuccess({
-        id: userSnapshot.id,
-        ...userSnapshot.data()
-      })
-    );
-  } catch (err) {
-    yield put(emailSignInFailure(err.message));
+    yield getUserSnapshot(user);
+  } catch (error) {
+    yield put(signInFailure(error.message));
   }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
 export function* onEmailSignIn() {
