8c6b07a8614bfd4d31caee79d15aa680a7eca1b5
Alaatohamy <alaatohamy4@gmail.com>
2019-09-23 14:47:29 +0200

    Feat: Add payment route that charge user through stripe Edit: Add post request to send user payment data to backend server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,4 +25,21 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
   console.log("Server is running on port " + port);
+});
+
+app.post("/payment", (req, res) => {
+  /** Data stripe api need to charge person */
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd"
+  };
+
+  stripe.charges.create(body, (stripErr, stripRes) => {
+    if (stripErr) {
+      res.status(500).send({ error: stripErr });
+    } else {
+      res.status(200).send({ success: stripRes });
+    }
+  });
 });
