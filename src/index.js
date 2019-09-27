0015d68d975ecdbba8cfdf2d015ae6d2641acb8f
Alaatohamy <alaatohamy4@gmail.com>
2019-09-27 09:05:42 +0200

    Feat: Add apollo client to the app with its config file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import './index.css';
-import 'styles/index.scss';
-import App from './App';
-import { store, persistor } from 'redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ApolloClient, gql } from "apollo-boost";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import "./index.css";
+import "styles/index.scss";
+import App from "./App";
+import { store, persistor } from "redux/store";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(res => console.log("res", res));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>, document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
