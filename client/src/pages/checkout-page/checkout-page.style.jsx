4735cdd22e2437501e9b0c95228326f0a2afa541
Alaatohamy <alaatohamy4@gmail.com>
2019-10-16 22:51:03 +0200

    Feat: Add responsive styling for checkout table

--- client/src/pages/checkout-page/checkout-page.style.jsx
+++ client/src/pages/checkout-page/checkout-page.style.jsx
@@ -3,8 +3,19 @@ import styled from "styled-components";
 export const StyledCheckoutPage = styled.section`
   min-width: 500px;
   width: 55%;
-  height: 90vh;
+  min-height: 90vh;
   margin: auto;
+  @media (max-width: 1024px) {
+    width: 70%;
+  }
+  @media (max-width: 850px) {
+    width: 95%;
+  }
+  @media (max-width: 600px) {
+    min-width: unset;
+    width: 100%;
+    text-align: right;
+  }
 `;
 
 export const Hint = styled.div`
@@ -12,4 +23,7 @@ export const Hint = styled.div`
   font-size: 24px;
   color: red;
   margin-top: 30px;
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
 `;
